1797f06f9f1b875c10706384338a0bb8d835f122
prpgift <prpgift@gmail.com>
2016-10-28 16:41:13 +0700

    gift update

--- store/models.py
+++ store/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-	username = models.CharField(max_length=20)
+	username = models.CharField(max_length=20,unique=True)
 	password = models.CharField(max_length=30)
 	isAdmin = models.BooleanField()
 	firstName = models.CharField(max_length=30)
@@ -13,3 +13,19 @@ class User(models.Model):
 	address = models.CharField(max_length=300)
 	tel = models.CharField(max_length=20)
 	company = models.CharField(max_length=50)
+			
+
+"""class Order(models.Model):
+	order_date = models.InteagerField(max_length=10)
+class Product(models.Model):
+	name_product = models.CharField(max_length=20)
+	amount_product = models.InteagerField(max_length=5)
+	price_product = models.DoubleField(max_length=5)
+	score_product = models.InteagerField(max_length=5)
+class Order_list(models.Model):
+	id_order = models.InteagerField(max_length=10)
+	id_product = models.InteagerField(max_length=10)
+class Track(models):
+	id_order = models.InteagerField(max_length=10)
+	current_state = models.CharField(max_length=10)
+	date_deliver = models.InteagerField(max_length=10)"""
--- store/views.py
+++ store/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response, redirect, get_object_or_404, HttpResponseRedirect
 from django.views import View
+from .models import *
 
 # Create your views here.
 class index(View):
         template_name = "store/index.html"
-
         def get(self, request):
+            obj, created = User.objects.get_or_create(username="admin", password="1234", isAdmin=True)
+            print created
             return render(request, self.template_name, {})
 

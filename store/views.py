89490058179a1b202840004a420cd45b3eb0af19
Pistachiology <pistachio.us@hotmail.com>
2016-10-28 03:23:37 -0700

    prepare urlpatterns and view

--- store/views.py
+++ store/views.py
@@ -5,9 +5,67 @@ from .models import *
 
 # Create your views here.
 class index(View):
-        template_name = "store/index.html"
-        def get(self, request):
-            obj, created = User.objects.get_or_create(username="admin", password="1234", isAdmin=True)
-            print created
-            return render(request, self.template_name, {})
+    template_name = "store/index.html"
+    def get(self, request):
+        # obj, created = User.objects.get_or_create(username="admin", password="1234", isAdmin=True)
+        return render(request, self.template_name, {})
 
+
+class register(View):
+    template_name = "store/register.html"
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+class all_product(View):
+    template_name = "store/all_product.html"
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+class product(View):
+    template_name = "store/product.html"
+
+    def get(self, request, product_id):
+        pass
+
+    def post(self, request, product_id):
+        pass
+
+class history(View):
+    template_name = "store/history.html"
+
+    def get(self, request):
+        pass
+
+class all_track(View):
+    template_name = "store/all_track.html"
+
+    def get(self, request):
+        pass
+
+class track(View):
+    template_name = "store/track.html"
+
+    def get(self, request, track_id):
+        pass
+
+class profile(View):
+    template_name = "store/profile.html"
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+class contact_us(View):
+    template_name = "store/contact_us.html"
+
+    def get(self, request):
+        pass

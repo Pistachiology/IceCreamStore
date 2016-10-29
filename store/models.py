22d805c35c4bd6bc22117ce40538e7b891ac1059
Nick <nick41746@hotmail.com>
2016-10-29 14:09:28 +0700

    update register

--- store/models.py
+++ store/models.py
@@ -13,7 +13,7 @@ class User(models.Model):
     address = models.CharField(max_length=300)
     tel = models.CharField(max_length=20)
     company = models.CharField(max_length=50)
-
+    
 class Product(models.Model):
     name_product = models.CharField(max_length=20)
     amount_product = models.IntegerField()
--- store/views.py
+++ store/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response, redirect, get_object_or_404, HttpResponseRedirect
 from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
-from models import User
 from django.db import connection
-#from .models import *
+from .models import *
 
 # Create your views here.
 class index(View):
@@ -16,39 +15,44 @@ class index(View):
 
 class register(View):
     template_name = "store/register.html"
-    err_message = ""
     def get(self, request):
         return render(request, self.template_name)
 
     def post(self, request):
-        err_occur = "The following errors occur:"
-        err_message = ""
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        repassword = request.POST.get('repassword', '')
-        firstname = request.POST.get('firstname', '')
-        lastname = request.POST.get('lastname', '')
-        address = request.POST.get('address', '')
-        tel = request.POST.get('tel', '')
-        company = request.POST.get('company', '')
-        if User.objects.filter(username=username).exists():
-            err_message += "<li>Username already exists</li>"
-        if password != repassword:
-            err_message += "<li>Password not match.</li>"
-        if firstname == "":
-            err_message += "<li>Null value at First Name</li>"
-        if lastname == "":
-            err_message += "<li>Null value at Last Name</li>"
-        if address == "":
-            err_message += "<li>Null value at Address</li>"
-        if tel == "":
-            err_message += "<li>Null value at Tel.</li>"
-        if err_message == "":
-            newUser = User(username=username, password=password, isAdmin=0, firstName=firstname, lastName=lastname, address=address, tel=tel, company=company)
+        response = {}
+        response['err_occur'] = "The following errors occur:"
+        response['err_message'] = ''
+        for key in request.POST.keys():
+            response[key] = request.POST.get(key, '')
+        if User.objects.filter(username=response['username']).exists():
+            response['err_message'] += "<li>Username already exists</li>"
+        if response['username'] == "":
+            response['err_message'] += "<li>Username is required</li>"
+        if response['password'] != response['repassword']:
+            response['err_message'] += "<li>Password not match.</li>"
+        if response['password'] == "":
+            response['err_message'] += "<li>Password is required</li>"
+        if response['firstname'] == "":
+            response['err_message'] += "<li>Null value at First Name</li>"
+        if response['lastname'] == "":
+            response['err_message'] += "<li>Null value at Last Name</li>"
+        if response['address'] == "":
+            response['err_message'] += "<li>Null value at Address</li>"
+        if response['tel'] == "":
+            response['err_message'] += "<li>Null value at Tel.</li>"
+        if response['err_message'] == "":
+            newUser = User(username=response['username'],
+                           password=response['password'],
+                           isAdmin=0,
+                           firstName=response['firstname'],
+                           lastName=response['lastname'],
+                           address=response['address'],
+                           tel=response['tel'],
+                           company=response['company'])
             newUser.save()
             return render(request, self.template_name)
         else:
-            return render(request, self.template_name, {'err_occur': err_occur, 'err_message': err_message})
+            return render(request, self.template_name, response)
 
 class all_product(View):
     template_name = "store/all_product.html"

1a637dd32b4d27117a5dc11eb20c21034cad87bf
Pistachiology <pistachio.us@hotmail.com>
2016-10-30 09:57:51 -0700

    register all models to admin site

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Product, CustomUser, Order, OrderList, Tracking, Cart
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(CustomUser)
+admin.site.register(Order)
+admin.site.register(OrderList)
+admin.site.register(Tracking)
+admin.site.register(Cart)

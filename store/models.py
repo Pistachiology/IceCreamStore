fa1ed8423705882e30955ec4127a2c418415766a
Nick <nick41746@hotmail.com>
2016-10-30 18:58:40 +0700

    fix cart backend

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
+from django.db.models.query import EmptyQuerySet
 
 class Product(models.Model):
     name = models.CharField(max_length=20)
@@ -31,34 +32,36 @@ class CustomUser(User):
     address = models.CharField(max_length=300)
     tel = models.CharField(max_length=20)
     company = models.CharField(max_length=50)
-    cart = models.ManyToManyField(Product, through='Cart')
+    user_cart = models.ManyToManyField(Product, through='Cart')
 
     def purchase(self):
-        order = Order(user_id=self.id,sum_price=0)
+        if self.user_cart.all().count() == 0:
+            return
+        order = Order(user=self,sum_price=0)
         order.save()
-        for item in self.cart:
-            product = Product.objects.get(id=item.product_id)
-            order.sum_price = item.qty * product.price
+        for product in self.user_cart.all():
+            item = Cart.objects.get(user=self, product=product)
+            order.sum_price += item.qty * product.price
             product.amount -= item.qty
             product.add_or_update()
-            OrderList(user_id=item.user_id, product_id=item.product_id, qty=item.qty).save()
-        self.cart.clear()
+            OrderList(order=order, product=product, qty=item.qty).save()
+        self.user_cart.clear()
         order.save()
     
 class Order(models.Model):
     date = models.DateField(auto_now_add=True)
     sum_price = models.FloatField()
     list_product = models.ManyToManyField(Product, through='OrderList')
-    user_id = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
 
 class OrderList(models.Model):
-    order_id = models.ForeignKey(Order,on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product,on_delete=models.CASCADE)
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
     qty = models.IntegerField()
 
 class Tracking(models.Model):
-    order_id = models.ForeignKey(Order,on_delete=models.CASCADE)
-    user_id = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     STATE_CHOICE = (
         (1 , 'Processing'),
         (2 , 'Preparing'),
@@ -68,12 +71,12 @@ class Tracking(models.Model):
     current_state = models.IntegerField(choices=STATE_CHOICE)
 
 class Cart(models.Model):
-    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     qty = models.IntegerField()
 
     def add_or_update(self):
-        obj, created = Cart.objects.update_or_create(user_id=self.user_id, product_id=self.product_id, defaults={'qty':self.qty})
+        obj, created = Cart.objects.update_or_create(user=self.user, product=self.product, defaults={'qty':self.qty})
         return created
     def delete(self):
-        Cart.objects.get(user_id=self.user_id, product_id=self.product_id).delete()
+        Cart.objects.get(user=self.user, product=self.product).delete()

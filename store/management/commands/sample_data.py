db68ea9678936596d5a374a7c0e170564c1c2883
Pistachiology <pistachio.us@hotmail.com>
2016-10-30 09:55:57 -0700

    sample_data command v2

--- store/management/commands/sample_data.py
+++ store/management/commands/sample_data.py
@@ -3,6 +3,34 @@ from store.models import Product
 import random
 import string
 
+flavour = [
+    'Chocolate Chip',
+    'Cookie Dough',
+    'Rainbow Sherbet',
+    'Vanilla',
+    'Chocolate Mint',
+    'Jamoca',
+    'Rocky Road',
+    'Very Berry Strawberry',
+    'Chocolate',
+    'Paline and Cream'
+]
+
+icecream_img = [
+    'choc-chip.jpg',
+    'cookie-dough.jpg', 
+    'RainbowSherbet.jpg',
+    'vanilla.jpg',
+    'choc-mint.png',
+    'jamoca.png',
+    'RockyRoad2.jpg',
+    'very-berry-stawberry.png',
+    'chocolate.png',
+    'paline-n-cream.png',
+    'showflavour.jpg'     
+]
+
+
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
@@ -11,7 +39,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         def random_name():
-            return "product_%s" % (''.join([ random.choice(string.letters) for i in range(10)] ))
+            return random.choice(flavour)
         def random_price():
             return random.randint(100, 300)
         def random_score():
@@ -20,9 +48,10 @@ class Command(BaseCommand):
             return random.randint(0, 100)
         def random_description():
             return "description_%s" % (''.join([ random.choice(string.letters) for i in range(20)] ))
-
+        def random_image():
+            return "/product_image/{icecream_img}".format(icecream_img=random.choice(icecream_img))
 
         for product_count in options['product_count']:
             for i in range(product_count):
-                product = Product(name=random_name(), price=random_price(), amount=random_amount(), score=random_score(), description=random_description())
+                product = Product(name=random_name(), price=random_price(), amount=random_amount(), score=random_score(), description=random_description(), image=random_image())
                 product.save()
